9304b4b9501fd1f578e5c5abe02a75f2de6e14ce
STARS7080 <andrey2004103@mail.ru>
2025-05-29 18:56:44 +0300

    Add files via upload

---
+++ Demo/AuthWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLogin_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+        
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+
+            if (string.IsNullOrWhiteSpace(login) || (string.IsNullOrWhiteSpace(password)))
+                {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+
+            try
+            {
+                using (var context = new DemoEntities())
+                {
+                    var user = context.Users
+                        .FirstOrDefault(u => u.Login == login && u.Pass ==password);
+
+                    if (user != null)
+                    {
+                        var role = user.Role?.RoleName ?? "неизвестный";
+                        MessageBox.Show($"Добро пожаловатьб {login}! ваша роль: {role}");
+
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный логин или пароль");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных" + ex.Message );
+            }
+
+        }
+    }
+}
---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DemoEntities _dbcontext = new DemoEntities();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            TovarsDataGid.ItemsSource = _dbcontext.Tovars.ToList();
+        }
+
+        
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newItem = new Tovars
+            {
+                Name = "Новый товар",
+                Discription = "Описание",
+                Price = 0
+            };
+
+            _dbcontext.Tovars.Add(newItem);
+            _dbcontext.SaveChanges();
+            LoadData();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TovarsDataGid.SelectedItem is Tovars selected)
+            {
+                selected.Name += " ";
+                _dbcontext.SaveChanges();
+                TovarsDataGid.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Выберите товар");
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            _dbcontext.Dispose();
+            _dbcontext = new DemoEntities();
+            LoadData();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TovarsDataGid.SelectedItem is Tovars selected)
+            {
+                _dbcontext.Tovars.Remove(selected);
+                _dbcontext.SaveChanges();
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Выберите товар");
+            }
+        }
+    }
+}
+
+
+
